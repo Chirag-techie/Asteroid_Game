46411744c252a12cc404431b28ab4e5d6d1fee6a
Chirag-techie <218345729+Chirag-techie@users.noreply.github.com>
2026-04-15 15:02:41 +0000

    B: The player is Moving scussfully

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pygame
+from player import Player
 from logger import log_state
 from constants import SCREEN_WIDTH
 from constants import SCREEN_HEIGHT
@@ -8,16 +9,29 @@ def main():
     print("Screen width:",SCREEN_WIDTH)
     print("Screen height:",SCREEN_HEIGHT)
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    updatable=pygame.sprite.Group()
+    drawable=pygame.sprite.Group()
+    updatable=pygame.sprite.Group()
+    drawable=pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+    clock=pygame.time.Clock()
+    dt=0
+    player=Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
     while True:
         log_state()
         for event in pygame.event.get():
-            pass
-        screen.fill("black")
-        pygame.display.flip()
-
-        for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        updatable.update(dt)
+
+        screen.fill("black")
+        
+        for shape in drawable:
+            shape.draw(screen)
+        
+        pygame.display.flip()
+        dt=clock.tick(60)/1000
+                
 
 if __name__ == "__main__":
     main()
