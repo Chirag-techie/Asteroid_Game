96c58dd0f9502f1a3a0f87b7554d4bb99c5dc0fa
Chirag-techie <218345729+Chirag-techie@users.noreply.github.com>
2026-04-16 11:12:14 +0000

    C:asteriod are created , moving and game is detecting collision and fixed gitignore file

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import pygame
-from player import Player
-from logger import log_state
+import sys
+from asteroid import *
+from asteroidfield import *
+from player import *
+from logger import *
 from constants import SCREEN_WIDTH
 from constants import SCREEN_HEIGHT
 def main():
@@ -9,20 +12,28 @@ def main():
     print("Screen width:",SCREEN_WIDTH)
     print("Screen height:",SCREEN_HEIGHT)
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    updatable=pygame.sprite.Group()
-    drawable=pygame.sprite.Group()
+    asteroids=pygame.sprite.Group()
     updatable=pygame.sprite.Group()
     drawable=pygame.sprite.Group()
     Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers=(updatable,)
+    asteroidfield=AsteroidField()
     clock=pygame.time.Clock()
     dt=0
     player=Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
+
     while True:
         log_state()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         updatable.update(dt)
+        for asteroid in asteroids:
+            if asteroid.collides_with(player):
+                log_event("player_hit")
+                print("Game over!")
+                sys.exit()
 
         screen.fill("black")
         
